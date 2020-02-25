cb0f97b9ba3e8aa8474acd58ec05cebb6225976a
VaibhavAgrawal11 <vaibhavagrawal1110@gmail.com>
2020-02-25 17:02:12 +0530

    MODIFIED: Added 4th airthmetic expression in program

--- airthmeticComputation.sh
+++ airthmeticComputation.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -x
-printf "Added 3rd airthmetic expression in program\n"
+printf "Added 4th airthmetic expression in program\n"
 printf "Enter three inputs: \n"
 read number1
 read number2
@@ -7,3 +7,4 @@ read number3
 result1=`echo "scale=3; $number1+$number2*$number3" | bc -l`
 result2=`echo "scale=3; $number1*$number2+$number3" | bc -l`
 result3=`echo "scale=3; $number3+$number1/$number2" | bc -l`
+result4=`echo "scale=3; $number1%$number2+$number3" | bc -l`
