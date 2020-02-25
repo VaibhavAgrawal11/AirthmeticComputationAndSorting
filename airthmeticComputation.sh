a291c08ee9c4afd4e4a4fed9d9bf734ba509559f
VaibhavAgrawal11 <vaibhavagrawal1110@gmail.com>
2020-02-25 17:22:23 +0530

    MODIFIED: Added feature to store result in dictionary

--- airthmeticComputation.sh
+++ airthmeticComputation.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -x
-printf "Added 4th airthmetic expression in program\n"
+printf "Added feature to store result in dictionary\n"
+declare -A resultDictionary
 printf "Enter three inputs: \n"
 read number1
 read number2
@@ -8,3 +9,7 @@ result1=`echo "scale=3; $number1+$number2*$number3" | bc -l`
 result2=`echo "scale=3; $number1*$number2+$number3" | bc -l`
 result3=`echo "scale=3; $number3+$number1/$number2" | bc -l`
 result4=`echo "scale=3; $number1%$number2+$number3" | bc -l`
+resultDictionary[1]="$result1"
+resultDictionary[2]="$result2"
+resultDictionary[3]="$result3"
+resultDictionary[4]="$result4"
