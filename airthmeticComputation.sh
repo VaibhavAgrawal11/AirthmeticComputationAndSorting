780111d03320a732386552f27ddf16d6a95e7025
VaibhavAgrawal11 <vaibhavagrawal1110@gmail.com>
2020-02-25 17:42:18 +0530

    MODIFIED: Added feature to convert dictionary values to store in array

--- airthmeticComputation.sh
+++ airthmeticComputation.sh
@@ -1,10 +1,12 @@
 #!/bin/bash -x
-printf "Added feature to store result in dictionary\n"
+printf "Added feature to convert dictionary values to store in array\n"
 declare -A resultDictionary
+#READING INPUTS FROM USER
 printf "Enter three inputs: \n"
 read number1
 read number2
 read number3
+#CALCULATING AIRTHMETIC OPERATIONS
 result1=`echo "scale=3; $number1+$number2*$number3" | bc -l`
 result2=`echo "scale=3; $number1*$number2+$number3" | bc -l`
 result3=`echo "scale=3; $number3+$number1/$number2" | bc -l`
@@ -13,3 +15,9 @@ resultDictionary[1]="$result1"
 resultDictionary[2]="$result2"
 resultDictionary[3]="$result3"
 resultDictionary[4]="$result4"
+#TAKING VALUES OF DICTIONARY IN ARRAY
+for value in ${!resultDictionary[@]}
+do
+	resultArray[$value]="${resultDictionary[$value]}"
+done
+echo "${resultArray[@]}"
